1a117467c73468a6252cb70ac16625921c42774b
GanisAD <122375116+GanisAD@users.noreply.github.com>
2026-05-21 09:51:02 +0700

    Add 'nama' column to users table in database

--- config/database.sql
+++ config/database.sql
@@ -2,6 +2,7 @@ USE lol_db;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
     role ENUM('mahasiswa', 'dosen') NOT NULL 
